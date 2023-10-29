59f03595fdff924b2e78b88136b1c11c3e04175d
Andrei <coursestud@mail.ru>
2023-10-29 14:48:17 +0300

    docs: add tailwind

--- src/1_app/App.tsx
+++ src/1_app/App.tsx
@@ -1,10 +1,7 @@
+import './styles/index.css';
 
 const App = () => {
-  return (
-    <div>
-      App
-    </div>
-  );
+  return <div className='text-lg text-red-600'>App</div>;
 };
 
 export default App;
