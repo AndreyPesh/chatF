23a096df60ce2d5797f426d98de45347813dbee2
Andrei <coursestud@mail.ru>
2023-10-29 22:08:47 +0300

    feat: add styles

--- src/4_features/listChat/ListChat.tsx
+++ src/4_features/listChat/ListChat.tsx
@@ -1,12 +1,24 @@
+import { useState } from 'react';
 import PreviewChat from './ui/PreviewChat';
 
 const fakeArrayChat = Array(5).fill('');
 
 const ListChat = () => {
+  const [currentChat, setCurrentChat] = useState<number | null>(null);
+
+  // const openChatHandler = (id: number) => {
+  //   console.log(id);
+  // };
+
   return (
     <div className="max-w-[370px]">
       {fakeArrayChat.map((_, index) => (
-        <PreviewChat key={index} isActive={Math.random() > 0.5} />
+        <PreviewChat
+          key={index}
+          id={index}
+          isActive={currentChat === index}
+          openChatHandler={setCurrentChat}
+        />
       ))}
     </div>
   );
--- src/4_features/listChat/ui/PreviewChat.tsx
+++ src/4_features/listChat/ui/PreviewChat.tsx
@@ -1,15 +1,28 @@
+import { Dispatch, FC, SetStateAction } from 'react';
 import classNames from 'classnames';
 
-const PreviewChat = ({ isActive }: { isActive: boolean }) => {
+interface PreviewChatProps {
+  id: number;
+  isActive: boolean;
+  openChatHandler: Dispatch<SetStateAction<number | null>>;
+}
+
+const PreviewChat: FC<PreviewChatProps> = ({
+  id,
+  isActive,
+  openChatHandler,
+}) => {
   return (
     <div
+      onClick={() => openChatHandler(id)}
       className={classNames('pl-6 pt-6 pr-6 cursor-pointer', {
         'border-l-[6px] border-l-nephritis bg-light-nephritis': isActive,
+        'border-l-[6px] border-l-white': !isActive,
       })}
     >
       <div className="pb-6 flex border-b border-b-light">
         <div className="w-[58px] h-[58px] rounded-full overflow-hidden">
-          <img src="/public/avatars/Avatar1.png" alt="user" />
+          <img src="/avatars/Avatar1.png" alt="user" />
         </div>
         <div className="ml-[10px] max-w-[195px]">
           <h2 className="font-bold font-4">Cameron Williamson</h2>
@@ -17,7 +30,7 @@ const PreviewChat = ({ isActive }: { isActive: boolean }) => {
             Not too bad, just trying to catch up on some work. How about you?
           </p>
         </div>
-        <div className="pl-[35px] flex flex-col justify-between">
+        <div className="pl-[28px] flex flex-col justify-between">
           <span className="font-medium text-concrete">2h</span>
           <span className="inline-flex w-6 h-6">xx</span>
         </div>
