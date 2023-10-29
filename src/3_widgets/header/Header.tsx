59ea99b412b973d0d56823768b7a5c865f08f13a
Andrei <coursestud@mail.ru>
2023-10-29 16:51:18 +0300

    feat: add styles to header

--- src/3_widgets/header/Header.tsx
+++ src/3_widgets/header/Header.tsx
@@ -2,12 +2,52 @@ import { NavLink } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <header>
-      <NavLink to="/">Main</NavLink>
-      <NavLink to="/messages">Messages</NavLink>
-      <NavLink to="/contacts">Contacts</NavLink>
+    <header className="p-2">
+      <NavLink
+        to="/"
+        className={({ isActive }) =>
+          stylesIcon({
+            isActive,
+            imageBg: 'bg-profile',
+            activeImageBg: 'bg-profile_active',
+          })
+        }
+      ></NavLink>
+      <NavLink
+        to="/messages"
+        className={({ isActive }) =>
+          stylesIcon({
+            isActive,
+            imageBg: 'bg-messages',
+            activeImageBg: 'bg-messages_active',
+          })
+        }
+      ></NavLink>
+      <NavLink
+        to="/contacts"
+        className={({ isActive }) =>
+          stylesIcon({
+            isActive,
+            imageBg: 'bg-contacts',
+            activeImageBg: 'bg-contacts_active',
+          })
+        }
+      ></NavLink>
     </header>
   );
 };
 
 export default Header;
+
+const stylesIcon = ({
+  isActive,
+  imageBg,
+  activeImageBg,
+}: {
+  isActive: boolean;
+  imageBg: string;
+  activeImageBg: string;
+}) => {
+  const base_styles = `inline-flex w-[52px] h-[52px] rounded-full ${imageBg} bg-no-repeat bg-center hover:bg-grayish`;
+  return isActive ? base_styles + ` ${activeImageBg} bg-grayish` : base_styles;
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'grayish': "#EAF1F1"
+      },
+      backgroundImage: {
+        'messages': "url('/public/icons/letter.svg')",
+        'messages_active': "url('/public/icons/letter_active.svg')",
+        'contacts': "url('/public/icons/contacts.svg')",
+        'contacts_active': "url('/public/icons/contacts_active.svg')",
+        'profile': "url('/public/icons/profile.svg')",
+        'profile_active': "url('/public/icons/profile_active.svg')",
+      }
+    },
   },
   plugins: [],
 }
