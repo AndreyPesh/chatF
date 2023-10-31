f800e13d5060c465bf098e3ea051a6b010ed4e90
Andrei <coursestud@mail.ru>
2023-10-31 16:43:08 +0300

    refactor: change type message data

--- src/6_shared/interfaces/message.ts
+++ src/6_shared/interfaces/message.ts
@@ -1,6 +1,7 @@
 import { ROLE_DIALOGUE_PARTICIPANTS } from '../enums/roles';
 
 export interface MessageData {
+  id: number;
   role: ROLE_DIALOGUE_PARTICIPANTS;
   userPhotoUrl: string;
   time: string;
