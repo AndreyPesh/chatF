188ec39f54c5e4f5d224cbdb72eab8d1952881a5
Andrei <coursestud@mail.ru>
2023-11-01 18:27:20 +0300

    feat: add animation

--- src/3_widgets/interlocutor/InterlocutorDescription.tsx
+++ src/3_widgets/interlocutor/InterlocutorDescription.tsx
@@ -3,15 +3,25 @@ import ImageList from '../../4_features/interlocutor/ImageList';
 import ListRecentFiles from '../../4_features/interlocutor/ListRecentFiles';
 import Notifications from '../../4_features/interlocutor/Notifications';
 import useInterlocutorDescriptionState from '../../6_shared/hooks/useInterlocutorDescriptionState';
+import classNames from 'classnames';
 
 const InterlocutorDescription = () => {
-  const { closeInterlocutorDescription } = useInterlocutorDescriptionState();
+  const { isShow, closeInterlocutorDescription } =
+    useInterlocutorDescriptionState();
   return (
-    <div className="relative p-8 pt-20 w-[372px] border-l-2 border-l-light">
-      <span
-        onClick={() => closeInterlocutorDescription()}
-        className="absolute right-8 top-8 inline-flex w-14 h-14 rounded-full bg-cross bg-no-repeat bg-center border-2 border-light transition-all cursor-pointer active:scale-90"
-      ></span>
+    <div
+      className={classNames(
+        'absolute p-8 pt-10 w-[372px] bg-white border-l-2 border-l-light border-r-2 border-r-light transition-all',
+        {'right-0': isShow},
+        {'right-[-200%]': !isShow},
+      )}
+    >
+      <div className="w-full flex justify-end">
+        <span
+          onClick={() => closeInterlocutorDescription()}
+          className="right-8 top-8 inline-flex w-14 h-14 rounded-full bg-cross bg-no-repeat bg-center border-2 border-light transition-all cursor-pointer active:scale-90"
+        ></span>
+      </div>
       <InterlocutorInfo />
       <Notifications />
       <ListRecentFiles />
--- src/4_features/client/ClientCaption.tsx
+++ src/4_features/client/ClientCaption.tsx
@@ -1,15 +1,18 @@
-import useInterlocutorDescriptionState from "../../6_shared/hooks/useInterlocutorDescriptionState";
-import StatusClient from "../statusClient/StatusClient";
-import ActionClient from "./ui/ActionClient";
+import useInterlocutorDescriptionState from '../../6_shared/hooks/useInterlocutorDescriptionState';
+import StatusClient from '../statusClient/StatusClient';
+import ActionClient from './ui/ActionClient';
 
 const ClientCaption = () => {
   const { showInterlocutorDescription } = useInterlocutorDescriptionState();
   return (
-    <div onClick={showInterlocutorDescription} className="py-6 px-8 flex w-full">
-      <div className="w-[52px] h-[52px] rounded-full overflow-hidden">
+    <div className="py-6 px-8 flex w-full">
+      <div
+        onClick={showInterlocutorDescription}
+        className="w-[52px] h-[52px] rounded-full overflow-hidden cursor-pointer"
+      >
         <img src="/avatars/Robert.png" alt="Client photo" />
       </div>
-      <div className="pl-[10px] grow">
+      <div onClick={showInterlocutorDescription} className="pl-[10px] grow cursor-pointer">
         <h2 className="font-bold">Robert Fox</h2>
         <StatusClient />
       </div>
