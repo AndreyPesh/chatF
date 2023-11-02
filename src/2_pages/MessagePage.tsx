080ca65609da8167b3b648db29d66daab0056516
Andrei <coursestud@mail.ru>
2023-11-02 16:41:10 +0300

    refactor: refactor

--- src/2_pages/MessagePage.tsx
+++ src/2_pages/MessagePage.tsx
@@ -1,13 +1,9 @@
 import Chats from '../3_widgets/chats/Chats';
-import Conversation from '../3_widgets/conversation/Conversation';
-import InterlocutorDescription from '../3_widgets/interlocutor/InterlocutorDescription';
 
 const MessagePage = () => {
   return (
-    <div className="relative flex w-full h-[100vh] border-r border-light overflow-hidden">
+    <div className="relative flex w-full h-[100vh] border-r border-light ">
       <Chats />
-      <Conversation />
-      <InterlocutorDescription />
     </div>
   );
 };
--- src/3_widgets/chats/Chats.tsx
+++ src/3_widgets/chats/Chats.tsx
@@ -3,16 +3,22 @@ import SearchField from '../../4_features/searchField/SearchField';
 import Button from '../../6_shared/buttons/Button';
 import { TYPE_BUTTON } from '../../6_shared/enums/buttons';
 import Title from '../../6_shared/titles/Title';
+import Conversation from '../conversation/Conversation';
+import InterlocutorDescription from '../interlocutor/InterlocutorDescription';
 
 const Chats = () => {
   return (
-    <div>
-      <Title>Messages</Title>
-      <div className='p-5 w-[380px]'>
-        <SearchField />
-        <Button sign="Start New Chat" type={TYPE_BUTTON.NEPHRITE} />
+    <div className="relative flex w-full overflow-hidden">
+      <div>
+        <Title>Messages</Title>
+        <div className="p-5 w-[380px]">
+          <SearchField />
+          <Button sign="Start New Chat" type={TYPE_BUTTON.NEPHRITE} />
+        </div>
+        <ListChat />
       </div>
-      <ListChat />
+      <Conversation />
+      <InterlocutorDescription />
     </div>
   );
 };
