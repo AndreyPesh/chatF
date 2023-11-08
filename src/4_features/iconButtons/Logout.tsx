0d4b31fb918ac547c74852fe777de3e68abcfeb3
Andrei <coursestud@mail.ru>
2023-11-08 22:20:51 +0300

    feat: add logout user

--- src/4_features/iconButtons/Logout.tsx
+++ src/4_features/iconButtons/Logout.tsx
@@ -1,6 +1,12 @@
+import { logoutUserFetch } from '../../6_shared/api/auth/auth';
+
 const Logout = () => {
+  const logoutHandler = async () => {
+    await logoutUserFetch();
+  };
+
   return (
-    <div>
+    <div onClick={logoutHandler}>
       <span className="inline-flex w-[52px] h-[52px] bg-logout bg-no-repeat bg-center rounded-full cursor-pointer hover:bg-grayish hover:shadow-lg"></span>
     </div>
   );
