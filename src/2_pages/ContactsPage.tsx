fc498f227c36bd278cc3aee34ac9ea90952a0436
Andrei <coursestud@mail.ru>
2023-11-08 16:46:27 +0300

    refactor: add test request with tokens

--- src/2_pages/ContactsPage.tsx
+++ src/2_pages/ContactsPage.tsx
@@ -1,7 +1,17 @@
+import { axiosInstance } from '../6_shared/axios/axiosInstance';
+
 const ContactsPage = () => {
+  const fetchUser = async () => {
+    const user = await axiosInstance(
+      '/user/05d85b68-98de-4990-8de0-0ea8fe49b649'
+    );
+    console.log(user);
+  };
+
   return (
     <>
       <div>Contacts</div>
+      <button onClick={fetchUser}>Get user</button>
     </>
   );
 };
--- src/6_shared/axios/axiosInstance.ts
+++ src/6_shared/axios/axiosInstance.ts
@@ -5,4 +5,5 @@ export const API_URL = 'http://localhost:5000/api';
 export const axiosInstance = axios.create({
   baseURL: API_URL,
   timeout: 1000,
+  withCredentials: true,
 });
