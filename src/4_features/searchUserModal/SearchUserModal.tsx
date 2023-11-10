0568e42106f310f5fe84e0c7d4a854c8efd35b1b
Andrei <coursestud@mail.ru>
2023-11-10 19:12:34 +0300

    feat: add preview participant

--- src/4_features/searchUserModal/SearchUserModal.tsx
+++ src/4_features/searchUserModal/SearchUserModal.tsx
@@ -1,7 +1,7 @@
 import Modal from '../../5_entities/modal/Modal';
 import useSearchUserModalStore from './state/state';
 import useQueryParticipant from '../../6_shared/hooks/useQueryParticipant';
-import ParticipantPreview from '../../5_entities/paticipant/ParticipantPreview';
+import ParticipantPreview from '../../5_entities/participant/ParticipantPreview';
 
 const SearchUserModal = () => {
   const { participantList } = useQueryParticipant();
