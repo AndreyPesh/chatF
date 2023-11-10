0f856406380340b11359c916afdd62f061c6d91d
Andrei <coursestud@mail.ru>
2023-11-10 18:56:58 +0300

    feat: add participant preview

--- src/4_features/searchUserModal/SearchUserModal.tsx
+++ src/4_features/searchUserModal/SearchUserModal.tsx
@@ -1,6 +1,7 @@
 import Modal from '../../5_entities/modal/Modal';
 import useSearchUserModalStore from './state/state';
 import useQueryParticipant from '../../6_shared/hooks/useQueryParticipant';
+import ParticipantPreview from '../../5_entities/paticipant/ParticipantPreview';
 
 const SearchUserModal = () => {
   const { participantList } = useQueryParticipant();
@@ -9,9 +10,7 @@ const SearchUserModal = () => {
     <Modal management={useSearchUserModalStore}>
       <h1>Search user</h1>
       {participantList?.map((participant) => (
-        <h1 className='underline'>
-          {participant.firstName} {participant.lastName}
-        </h1>
+        <ParticipantPreview data={participant} />
       ))}
     </Modal>
   );
---
+++ src/5_entities/paticipant/ParticipantPreview.tsx
@@ -0,0 +1,23 @@
+import { FC } from 'react';
+import { Participant } from '../../6_shared/api/user/interfaces/user';
+
+interface ParticipantPreviewProps {
+  data: Participant;
+}
+
+const ParticipantPreview: FC<ParticipantPreviewProps> = ({ data }) => {
+  const { firstName, lastName } = data;
+  const initialsName = firstName.charAt(0) + lastName.charAt(0);
+  return (
+    <div className="p-2 my-1 min-w-[300px] flex border border-gray rounded-lg cursor-pointer transition-all active:scale-95">
+      <div className="w-12 h-12 inline-flex items-center justify-center rounded-full bg-nephritis text-white uppercase">
+        {initialsName}
+      </div>
+      <p className='pl-2 inline-flex items-center justify-center font-bold'>
+        {firstName} {lastName}
+      </p>
+    </div>
+  );
+};
+
+export default ParticipantPreview;
