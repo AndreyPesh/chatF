104d3d5de936c583ae068b042806c82f144138be
Andrei <coursestud@mail.ru>
2023-11-14 15:20:57 +0300

    feat: add test socket component

--- src/2_pages/ContactsPage.tsx
+++ src/2_pages/ContactsPage.tsx
@@ -1,25 +1,92 @@
-import userApi from '../6_shared/api/user/userApi';
-import { axiosInstance } from '../6_shared/axios/axiosInstance';
+import { Socket, io } from 'socket.io-client';
+import { ChatLayout } from '../testComponents/ChatLayout';
+import { Header } from '../testComponents/Header';
+import { MessageForm } from '../testComponents/MessageForm';
+import { Messages } from '../testComponents/Messages';
+import {
+  ClientToServerEvents,
+  Message,
+  ServerToClientEvents,
+  User,
+} from '../testComponents/interfaces/chat.interface';
+import { useEffect, useState } from 'react';
+import { LoginLayout } from '../testComponents/LoginLayout';
+import { LoginForm } from '../testComponents/LoginForm';
+
+const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io();
 
 const ContactsPage = () => {
-  const fetchUser = async () => {
-    const user = await axiosInstance(
-      '/user/05d85b68-98de-4990-8de0-0ea8fe49b649'
-    );
-    console.log(user);
-  };
+  const [isConnected, setIsConnected] = useState(socket.connected);
+  const [messages, setMessages] = useState<Message[]>([]);
+  const [user, setUser] = useState<User>({
+    userId: '',
+    userName: '',
+  });
 
-  const fetchParticipants = async () => {
-    const users = await userApi.getListParticipants()
-    console.log(users);
-    
-  } 
+  useEffect(() => {
+    const currentUser = JSON.parse(sessionStorage.getItem('user') ?? `{}`);
+    // If userId exists, we know there is a "logged in" user.
+    if (currentUser.userId) {
+      // So we set the current user into state.
+      setUser(currentUser);
+    }
+    socket.on('connect', () => {
+      // Set connect state accordingly.
+      setIsConnected(true);
+    });
+    socket.on('disconnect', () => {
+      // Set connect state accordingly.
+      setIsConnected(false);
+    });
+    socket.on('chat', (e) => {
+      // Set connect state accordingly.
+      setMessages((messages) => [e, ...messages]);
+    });
+  }, []);
+
+  const sendMessage = (e: React.FormEvent<HTMLFormElement>) => {
+    // There must be a user set in state to send a message.
+    if (user) {
+      // Given a user is "logged in", emit a 'chat' message to the server
+      // with the current user's information, the time of the message (timeSent),
+      // and the value of the user input in the message form.
+      socket.emit('chat', {
+        user: {
+          userId: user.userId,
+          userName: user.userName,
+        },
+        timeSent: new Date(Date.now()).toLocaleString('en-US'),
+        //@ts-ignore
+        message: e.target[0].value,
+      })
+    }
+  }
+
+  const login = (e: React.FormEvent<HTMLFormElement>) => {
+    //@ts-ignore
+    const formValue = e.target[0].value;
+    const newUser = {
+      userId: Date.now().toLocaleString().concat(formValue),
+      userName: formValue,
+    };
+    sessionStorage.setItem('user', JSON.stringify(newUser));
+    setUser(newUser);
+  };
 
   return (
     <>
-      <div>Contacts</div>
-      <button onClick={fetchUser}>Get user</button>
-      <button onClick={fetchParticipants}>Get list user</button>
+      {user && user.userId ? (
+        <ChatLayout>
+          <div>Contacts</div>
+          <Header user={user} isConnected={isConnected}></Header>
+          <Messages user={user} messages={messages}></Messages>
+          <MessageForm sendMessage={sendMessage}></MessageForm>
+        </ChatLayout>
+      ) : (
+        <LoginLayout>
+          <LoginForm login={login}></LoginForm>
+        </LoginLayout>
+      )}
     </>
   );
 };
