24ed4103ed59136bde6d2a7df3b0267d3f3143d3
Andrei <coursestud@mail.ru>
2023-11-16 21:39:20 +0300

    refactor: refactor

--- src/4_features/listChat/ListChat.tsx
+++ src/4_features/listChat/ListChat.tsx
@@ -1,21 +1,32 @@
 import { useEffect, useState } from 'react';
 // import PreviewChat from './ui/PreviewChat';
-// import useUserStore from '../../6_shared/hooks/store/useUserStore';
+import useUserStore from '../../6_shared/hooks/store/useUserStore';
 import { socket } from '../../6_shared/socket/socket';
 import PreviewRoom from './ui/PreviewRoom';
 
 interface Room {
-  host: { unitName: string };
-  name: string;
+  id: string;
+  // host: { unitName: string };
+  roomName: string;
 }
 
 const ListChat = () => {
-  // const { user } = useUserStore();
+  const { user } = useUserStore();
   const [rooms, setRooms] = useState<Room[]>([]);
   // const { conversationList } = useQueryConversation(user.id);
 
+  const getRooms = () => {
+    console.log('get rooms');
+
+    socket.emit('list_rooms', { userId: user.id });
+  };
+
   useEffect(() => {
-    socket.emit('list_rooms');
+    if (user.id) {
+      console.log('emit ', user.id);
+
+      socket.emit('list_rooms', { userId: user.id });
+    }
 
     socket.on('rooms', (rooms: Room[]) => {
       console.log('received rooms ', rooms);
@@ -27,19 +38,22 @@ const ListChat = () => {
     return () => {
       socket.disconnect();
     };
-  }, []);
+  }, [user, user.id]);
+
+  // useEffect(() => {
+  //   if (socket.disconnected) socket.connect();
+  // }, []);
 
   return (
     <div className="max-w-[390px] pb-[260px] h-[100vh] overflow-y-auto scrollbar-thin scrollbar-thumb-gray-300 scrollbar-track-light">
       {rooms &&
         rooms.length > 0 &&
-        rooms.map((room) => (
-          <PreviewRoom key={room.host.unitName} name={room.name} />
-        ))}
+        rooms.map((room) => <PreviewRoom key={room.id} name={room.roomName} />)}
       {/* {conversationList &&
         conversationList.map((conversation) => (
           <PreviewChat key={conversation.id} conversation={conversation} />
         ))} */}
+      <button onClick={getRooms}>Get rooms</button>
     </div>
   );
 };
