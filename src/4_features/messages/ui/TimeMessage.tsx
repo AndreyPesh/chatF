b41517767efd66b8ab7ea46ba447ffb89f40ecc5
Andrei <coursestud@mail.ru>
2023-11-20 17:02:10 +0300

    refactor: add time component

--- src/4_features/messages/ui/TimeMessage.tsx
+++ src/4_features/messages/ui/TimeMessage.tsx
@@ -1,6 +1,6 @@
 const TimeMessage = ({ time }: { time: Date }) => {
   const localTime = new Date(time).toLocaleTimeString();
-  return <span className="ml-2 inline-flex text-concrete">{localTime}</span>;
+  return <span className="ml-2 inline-flex text-concrete text-sm">{localTime}</span>;
 };
 
 export default TimeMessage;
