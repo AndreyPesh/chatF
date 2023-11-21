ecaea1e8267b45f4b61db34f9df2b390d5dc63e6
Andrei <coursestud@mail.ru>
2023-11-21 14:54:38 +0300

    refactor: refactor

--- src/4_features/listChat/ui/UnreadMessageIndicator.tsx
+++ src/4_features/listChat/ui/UnreadMessageIndicator.tsx
@@ -1,33 +1,11 @@
-import { useEffect, useState } from 'react';
-import { useChatSocketCtx } from '../../../6_shared/socket/socketContext';
-import { CHAT_EVENTS } from '../../../6_shared/socket/types/events.enum';
 import classNames from 'classnames';
 
-const UnreadMessageIndicator = ({
-  value,
-}: {
-  value: number;
-  roomId: string;
-}) => {
-  const { socket } = useChatSocketCtx();
-  const [counterMessages, setCounterMessages] = useState(value);
-
-  useEffect(() => {
-    const readMessageHandler = () => {
-      console.log('message readed');
-      // setCounterMessages(0);
-    };
-    socket.on(CHAT_EVENTS.READ_MESSAGE, readMessageHandler);
-    return () => {
-      socket.off(CHAT_EVENTS.READ_MESSAGE, readMessageHandler);
-    };
-  }, []);
-
+const UnreadMessageIndicator = ({ value }: { value: number }) => {
   return (
     <div
       className={classNames(
         'w-6 h-6 inline-flex items-center justify-center bg-red text-white rounded-full',
-        // { 'hidden': counterMessages === 0}
+        { hidden: value === 0 }
       )}
     >
       {value}
